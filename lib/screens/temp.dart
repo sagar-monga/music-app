5ef808553e9140fc8bbabce150e6cc663379f39f
Sagar Monga <sagar.monga3097@gmail.com>
2021-04-14 19:50:33 +0530

    Code Cleanup

---
+++ lib/screens/temp.dart
@@ -0,0 +1,13 @@
+class MyWidget extends StatefulWidget {
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
